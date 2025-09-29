3ac6c6e5ecbf3c2cfa491e5ebd195f726a043449
Jeyvidisson <fbj86618@gmail.com>
2025-09-29 19:01:29 -0300

    atv

--- 2025-09-24 Aula/atv3.py
+++ 2025-09-24 Aula/atv3.py
@@ -1,3 +1,6 @@
+#Desenvolva um programa em Python que solicite ao usuário dois
+#valores inteiros e em seguida calcule a média aritmética entre eles.
+
 a = int(input('Primeiro valor'));
 b = int(input('Segundo valor'));
 media = (a + b) / 2
